1aefc358229ca0ac130b3011304c6b9ced6eeddd
Venikanya <veni458@gmail.com>
2020-10-04 23:48:14 -0700

    Initial Commit with scaffolding and hot loader

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => {
+  return (
+    <div>
+      hello!
+    </div>
+  );
+};
+
+export default hot(module)(App);
